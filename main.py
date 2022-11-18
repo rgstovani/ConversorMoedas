d1f40babe3af7951e39dafbdfb105fa31bbad9fa
rgstovani <113445318+rgstovani@users.noreply.github.com>
2022-11-17 23:35:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -18,9 +18,20 @@ cria_linha()
 print(f"{'Vamos configurar o cotador de Euro':^80}")
 cria_linha()
 
-preco_alvo = input('Digite o valor alvo: R$')
-tempo_de_atualizacao = input('Digite o tempo de atualização (em segundos):')
-tempo = int(tempo_de_atualizacao)
+while True:
+    try:
+        preco_alvo = float(input('Digite o valor desejado: R$'))
+        break
+    except ValueError:
+        print('Valor Invalido. Use o ponto para casas decimais.')
+
+while True:
+    tempo_de_atualizacao = input('Digite o tempo de atualização (em segundos):')
+    if tempo_de_atualizacao.isnumeric():
+        tempo = int(tempo_de_atualizacao)
+        break
+    else:
+        print('Digite apenas numeros.!')
 
 cria_linha2()
 print(f"{'Agora você pode minimizar, te avisaremos quando atigir o valor desejado.':^80}")
@@ -29,7 +40,7 @@ cria_linha2()
 while True:
     requisicao = requests.get('https://economia.awesomeapi.com.br/all/EUR-BRL')
     cotacao = requisicao.json()
-    euro = cotacao['EUR']['bid']
+    euro = float(cotacao['EUR']['bid'])
     print('Atualizado em: ', datetime.now().strftime("%d-%m-%y %H:%M:%S"))
     print('Cotação Atual:', euro)
     if (euro <= preco_alvo):
@@ -39,4 +50,3 @@ while True:
         print('Ainda está caro!')
         cria_linha2()
     time.sleep(tempo)
-
